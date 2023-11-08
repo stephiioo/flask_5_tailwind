6e7706f63a7b79cdfff8b93ab6233b8dc0a3e032
stephiioo <stephihiioo@gmail.com>
2023-11-08 11:28:22 +0000

    update

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# small changes for push 12345
